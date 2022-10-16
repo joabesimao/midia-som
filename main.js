6687713f7a3bed2b6962bc45ad242e9a4cae9dbf
joabesimao <simaojoabecosta@gmail.com>
2022-10-15 21:02:58 -0300

    deleta funcao inline

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 //document.querySelector("input[type =tel]");
 
-function tocaSom(tecla) {
-  document.querySelector(tecla).play();
+function tocaSom(idElement) {
+  document.querySelector(idElement).play();
 }
 
-//document.querySelector(".tecla_pom");
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  contador++;
+}
